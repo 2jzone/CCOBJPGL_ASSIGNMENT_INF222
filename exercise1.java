9242d2016849bb460820f080a7c5e4d3723db975
Jason Angeles <129362279+2jzone@users.noreply.github.com>
2023-04-20 14:29:43 +0800

    Update exercise1.java

--- exercise1.java
+++ exercise1.java
@@ -5,7 +5,7 @@ public class exercise1 {
     public static void main(String[] args) throws Exception {
 
         //precondition
-        assert iWillBehave == true : "You're a very bad boy";
+        assert iWillBehave == true : "Sit down be humble - pareng kendrick";
 
         College();
 
