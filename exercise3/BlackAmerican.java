c1cade8f18b44dc887e9aa82ac7e23ff35ed110e
Jason Angeles <129362279+2jzone@users.noreply.github.com>
2023-05-11 14:54:17 +0800

    Delete BlackAmerican.java

--- exercise3/BlackAmerican.java
+++
@@ -1,9 +0,0 @@
-package Dog;
-class BlackAmerican extends BlackRussianTerrier implements Americanbulldog {
-    public void bark(){ 
-        System.out.println("The Black American is barking!");
-    }
-    public void hide(){
-        System.out.println("The Black American is hiding from the dog police");
-    }
-}
